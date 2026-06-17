c89af3b5d474e95ed96ee771641d408b6b4ed0cd
Devesh Kumar Upadhayay <deveshkumarupadhayay@gmail.com>
2026-06-17 14:17:18 +0530

    Refactor Navbar component structure and ensure proper formatting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,10 @@
-<nav>
-<h1>"Devesh"</h1>
-</nav>
+function Navbar() {
+  return (
+    <nav>
+      <h1>Devesh</h1>
+      <h1>Portfolio</h1>
+    </nav>
+  );
+}
 
-export default Navbar
+export default Navbar;
